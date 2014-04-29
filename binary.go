7ff77d6b6ea9986872dc6ec91cc1ef90d9591d62
Alec Thomas <alec@swapoff.org>
2014-04-29 17:01:52 -0400

    General purpose binary encoder/decoder.

---
+++ binary.go
@@ -0,0 +1,243 @@
+package binary
+
+import (
+	"bufio"
+	"bytes"
+	"encoding"
+	"encoding/binary"
+	"errors"
+	"fmt"
+	"io"
+	"reflect"
+)
+
+var (
+	DefaultEndian = binary.LittleEndian
+)
+
+func Marshal(v interface{}) ([]byte, error) {
+	b := &bytes.Buffer{}
+	if err := NewEncoder(b).Encode(v); err != nil {
+		return nil, err
+	}
+	return b.Bytes(), nil
+}
+
+func Unmarshal(b []byte, v interface{}) error {
+	return NewDecoder(bytes.NewReader(b)).Decode(v)
+}
+
+type Encoder struct {
+	w io.Writer
+}
+
+func NewEncoder(w io.Writer) *Encoder {
+	return &Encoder{w}
+}
+
+func (e *Encoder) writeVarint(v int) error {
+	b := make([]byte, 8)
+	l := binary.PutUvarint(b, uint64(v))
+	b = b[:l]
+	_, err := e.w.Write(b)
+	return err
+}
+
+func (b *Encoder) Encode(v interface{}) (err error) {
+	switch cv := v.(type) {
+	case encoding.BinaryMarshaler:
+		buf, err := cv.MarshalBinary()
+		if err != nil {
+			return err
+		}
+		if err = b.writeVarint(len(buf)); err != nil {
+			return err
+		}
+		_, err = b.w.Write(buf)
+
+	case []byte: // fast-path byte arrays
+		if err = b.writeVarint(len(cv)); err != nil {
+			return
+		}
+		_, err = b.w.Write(cv)
+
+	case string:
+		if err = b.writeVarint(len(cv)); err != nil {
+			return
+		}
+		_, err = b.w.Write([]byte(cv))
+
+	case bool:
+		var out byte
+		if cv {
+			out = 1
+		}
+		err = binary.Write(b.w, DefaultEndian, out)
+
+	case int:
+		err = binary.Write(b.w, DefaultEndian, int64(cv))
+
+	case uint:
+		err = binary.Write(b.w, DefaultEndian, int64(cv))
+
+	case int8, uint8, int16, uint16, int32, uint32, int64, uint64, float32,
+		float64, complex64, complex128:
+		err = binary.Write(b.w, DefaultEndian, v)
+
+	default:
+		rv := reflect.Indirect(reflect.ValueOf(v))
+		t := rv.Type()
+		switch t.Kind() {
+		case reflect.Array, reflect.Slice:
+			l := rv.Len()
+			if err = b.writeVarint(l); err != nil {
+				return
+			}
+			for i := 0; i < l; i++ {
+				if err = b.Encode(rv.Index(i).Interface()); err != nil {
+					return
+				}
+			}
+
+		case reflect.Struct:
+			l := rv.NumField()
+			for i := 0; i < l; i++ {
+				if v := rv.Field(i); v.CanSet() && t.Field(i).Name != "_" {
+					if err = b.Encode(v.Interface()); err != nil {
+						return
+					}
+				}
+			}
+
+		case reflect.Map:
+			l := rv.Len()
+			if err = b.writeVarint(l); err != nil {
+				return
+			}
+			for _, key := range rv.MapKeys() {
+				value := rv.MapIndex(key)
+				if err = b.Encode(key.Interface()); err != nil {
+					return err
+				}
+				if err = b.Encode(value.Interface()); err != nil {
+					return err
+				}
+			}
+
+		default:
+			return errors.New("unsupported type " + t.String())
+		}
+	}
+	return
+}
+
+type Decoder struct {
+	r *bufio.Reader
+}
+
+func NewDecoder(r io.Reader) *Decoder {
+	return &Decoder{bufio.NewReader(r)}
+}
+
+func (d *Decoder) Decode(v interface{}) (err error) {
+	switch cv := v.(type) {
+	case *string:
+		var l uint64
+		if l, err = binary.ReadUvarint(d.r); err != nil {
+			return
+		}
+		buf := make([]byte, l)
+		_, err = d.r.Read(buf)
+		*cv = string(buf)
+
+	case *bool:
+		var out byte
+		err = binary.Read(d.r, DefaultEndian, &out)
+		*cv = out != 0
+
+	case *int:
+		var out int64
+		err = binary.Read(d.r, DefaultEndian, &out)
+		*cv = int(out)
+
+	case *uint:
+		var out uint64
+		err = binary.Read(d.r, DefaultEndian, &out)
+		*cv = uint(out)
+
+	case *int8, *uint8, *int16, *uint16, *int32, *uint32, *int64, *uint64, *float32,
+		*float64, *complex64, *complex128:
+		err = binary.Read(d.r, DefaultEndian, v)
+
+	default:
+		// Check if the type implements the encoding.BinaryUnmarshaler interface, and use it if so.
+		if i, ok := v.(encoding.BinaryUnmarshaler); ok {
+			var l uint64
+			if l, err = binary.ReadUvarint(d.r); err != nil {
+				return
+			}
+			buf := make([]byte, l)
+			_, err = d.r.Read(buf)
+			return i.UnmarshalBinary(buf)
+		}
+
+		// Otherwise, use reflection.
+		rv := reflect.Indirect(reflect.ValueOf(v))
+		if !rv.CanAddr() {
+			return errors.New("can only Decode to pointer type")
+		}
+		t := rv.Type()
+
+		switch t.Kind() {
+		case reflect.Array, reflect.Slice:
+			var l uint64
+			if l, err = binary.ReadUvarint(d.r); err != nil {
+				return
+			}
+			if t.Kind() == reflect.Slice {
+				rv.Set(reflect.MakeSlice(t, int(l), int(l)))
+			} else if int(l) != t.Len() {
+				return fmt.Errorf("encoded size %d != real size %d", l, t.Len())
+			}
+			for i := 0; i < int(l); i++ {
+				if err = d.Decode(rv.Index(i).Addr().Interface()); err != nil {
+					return
+				}
+			}
+
+		case reflect.Struct:
+			l := rv.NumField()
+			for i := 0; i < l; i++ {
+				if v := rv.Field(i); v.CanSet() && t.Field(i).Name != "_" {
+					if err = d.Decode(v.Addr().Interface()); err != nil {
+						return
+					}
+				}
+			}
+
+		case reflect.Map:
+			var l uint64
+			if l, err = binary.ReadUvarint(d.r); err != nil {
+				return
+			}
+			kt := t.Key()
+			vt := t.Elem()
+			rv.Set(reflect.MakeMap(t))
+			for i := 0; i < int(l); i++ {
+				kv := reflect.Indirect(reflect.New(kt))
+				if err = d.Decode(kv.Addr().Interface()); err != nil {
+					return
+				}
+				vv := reflect.Indirect(reflect.New(vt))
+				if err = d.Decode(vv.Addr().Interface()); err != nil {
+					return
+				}
+				rv.SetMapIndex(kv, vv)
+			}
+
+		default:
+			return errors.New("unsupported type " + t.String())
+		}
+	}
+	return
+}
---
+++ binary_test.go
@@ -0,0 +1,107 @@
+package binary
+
+import (
+	"errors"
+	"testing"
+	"time"
+
+	"github.com/stretchrcom/testify/assert"
+)
+
+type s0 struct {
+	A string
+	B string
+	C int16
+}
+
+var (
+	s0v = &s0{"A", "B", 1}
+	s0b = []byte{0x1, 0x41, 0x1, 0x42, 0x1, 0x0}
+)
+
+func TestBinaryEncodeStruct(t *testing.T) {
+	b, err := Marshal(s0v)
+	assert.NoError(t, err)
+	assert.Equal(t, s0b, b)
+}
+
+func TestBinaryDecodeStruct(t *testing.T) {
+	s := &s0{}
+	err := Unmarshal(s0b, s)
+	assert.NoError(t, err)
+	assert.Equal(t, s0v, s)
+}
+
+func TestBinaryDecodeToValueErrors(t *testing.T) {
+	b := []byte{1, 0, 0, 0}
+	var v uint32
+	err := Unmarshal(b, v)
+	assert.Error(t, err)
+	err = Unmarshal(b, &v)
+	assert.NoError(t, err)
+	assert.Equal(t, uint32(1), v)
+}
+
+type s1 struct {
+	Name     string
+	BirthDay time.Time
+	Phone    string
+	Siblings int
+	Spouse   bool
+	Money    float64
+	Tags     map[string]string
+	Aliases  []string
+}
+
+var (
+	s1v = &s1{
+		Name:     "Bob Smith",
+		BirthDay: time.Date(2013, 1, 2, 3, 4, 5, 6, time.UTC),
+		Phone:    "5551234567",
+		Siblings: 2,
+		Spouse:   false,
+		Money:    100.0,
+		Tags:     map[string]string{"key": "value"},
+		Aliases:  []string{"Bobby", "Robert"},
+	}
+
+	svb = []byte{0x9, 0x42, 0x6f, 0x62, 0x20, 0x53, 0x6d, 0x69, 0x74, 0x68, 0xf,
+		0x1, 0x0, 0x0, 0x0, 0xe, 0xc8, 0x75, 0x9a, 0xa5, 0x0, 0x0, 0x0, 0x6, 0xff,
+		0xff, 0xa, 0x35, 0x35, 0x35, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x2,
+		0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x59,
+		0x40, 0x1, 0x3, 0x6b, 0x65, 0x79, 0x5, 0x76, 0x61, 0x6c, 0x75, 0x65, 0x2, 0x5,
+		0x42, 0x6f, 0x62, 0x62, 0x79, 0x6, 0x52, 0x6f, 0x62, 0x65, 0x72, 0x74}
+)
+
+func TestBinaryEncodeComplex(t *testing.T) {
+	b, err := Marshal(s1v)
+	assert.NoError(t, err)
+	assert.Equal(t, svb, b)
+	s := &s1{}
+	err = Unmarshal(svb, s)
+	assert.NoError(t, err)
+	assert.Equal(t, s1v, s)
+}
+
+type s2 struct {
+	b []byte
+}
+
+func (s *s2) UnmarshalBinary(data []byte) error {
+	if len(data) != 1 {
+		return errors.New("expected data to be length 1")
+	}
+	s.b = data
+	return nil
+}
+
+func (s *s2) MarshalBinary() (data []byte, err error) {
+	return s.b, nil
+}
+
+func TestBinaryMarshalUnMarshaler(t *testing.T) {
+	s2v := &s2{[]byte{0x13}}
+	b, err := Marshal(s2v)
+	assert.NoError(t, err)
+	assert.Equal(t, []byte{0x1, 0x13}, b)
+}
